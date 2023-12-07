d4dabbe93666416494b46158a8cd01e705c69271
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2023-12-06 21:26:38 -0300

    add dark and light mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,12 @@
 import { UserButton } from '@clerk/nextjs'
 
+import { ModeToggle } from '@/components/mode-toggle'
+
 export default function Home() {
   return (
     <div className="flex flex-col">
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   )
 }
