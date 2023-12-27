b7792fc8af0c3c4baca199f1377275d6e8ec6a83
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2023-12-27 17:43:36 -0300

    add chat page header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,53 @@
-function ChannelPage() {
-  return <div>Channel ID Page</div>
+import { redirect } from 'next/navigation'
+import { redirectToSignIn } from '@clerk/nextjs'
+
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db'
+
+import { ChatHeader } from '@/components/chat/chat-header'
+
+type ChannelPageProps = {
+  params: {
+    channelId: string
+    serverId: string
+  }
+}
+
+const ChannelPage = async ({
+  params: { channelId, serverId }
+}: ChannelPageProps) => {
+  const profile = await currentProfile()
+
+  if (!profile) {
+    return redirectToSignIn()
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId
+    }
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId,
+      profileId: profile.id
+    }
+  })
+
+  if (!channel || !member) {
+    redirect('/')
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  )
 }
 
 export default ChannelPage
